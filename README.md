0419fc5239c01cebff475ed165068b2fa39d475f
王悦 <1124233930@qq.com>
2020-01-23 21:07:00 +0800

    xiugai

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # 这是一个项目
 # author:wy\
-# 
+# 第一次修改
+# 用传统的方式把修改后的代码上传到码云上
+## 1.命令行 git add .          git commit -m ""         git push -u origin master
+## 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 //入口文件
 import Vue from 'vue'		//导入Vue包
 
-import app from './app.vue'		//导入app组件
+import app from './app.vue'		//导入app组件 
 
 import { Header } from 'mint-ui'  //导入nint-ui需要的组件包及样式,并使用注册
 import 'mint-ui/lib/style.css'
